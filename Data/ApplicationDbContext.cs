117012646f7b0eed9246f0ee26824bca235948a7
BarwSirati <sirati10546@gmail.com>
2022-12-19 22:03:44 +0700

    downgrade version && add migration

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+namespace Backend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+            base(options)
+        {
+
+        }
+
+        public DbSet<Users> User { get; set; }
+    }
+}
--- Models/Users.cs
+++ Models/Users.cs
@@ -1,9 +1,25 @@
 ﻿namespace Backend.Models
 {
+    [Index(nameof(Username), IsUnique = true)]
+
     public class Users
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(maximumLength: 100)]
+        public String Name { get; set; }
+        [Required(ErrorMessage = "Lastname is required.")]
+        [StringLength(maximumLength: 100)]
+        public String Lastname { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(maximumLength: 100)]
         public String Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(maximumLength: 100, MinimumLength = 10)]
         public String Password { get; set; }
     }
 }
